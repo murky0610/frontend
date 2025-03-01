28d844e87e9b79ecaf411a224aa74a9c936660fb
murky0610 <murkymurky0@gmail.com>
2025-03-01 16:19:14 +0800

    fix: improve login and register UI

--- src/app/(unauth)/(credentials)/login/page.tsx
+++ src/app/(unauth)/(credentials)/login/page.tsx
@@ -1,116 +1,34 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 'use client'
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Image from "next/image";
-import { useState } from "react";
-import { userLogin } from "@/app/endpoint/api";
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/components/LoginForm"
 
-import { useRouter } from "next/navigation"; 
 
-export default function LoginPage() { 
-    const [email, setUserEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter(); 
+export default function LoginPage() {
 
-    const handleLogin = async () => {
-        const response = await userLogin(email, password);
-
-        if (response === true) {
-            router.push('/dashboard'); 
-        }
-    };
-    return (
-        
-        <div className="flex items-center justify-center m-20">
-           
-        {/* LEFT SIDE OF COLUMN */}
- 
-        <div className="w-full min-w-lg  flex items-center justify-center">
-          <div className="w-full flex flex-col items-center justify-center p-10">
-          <Image src="/logo-v2.png" // Ensure the image is placed in `public/`
-          alt="AAVC Right Column"
-          width={300} // Adjust width as needed
-          height={200} // Adjust height as needed
-          className="object-cover" />
-            <div className="w-full p-5 text-center">
-              <h1 className="text-3xl md:text-4xl font-bold">
-                Let's Login to your AAVC Lab Account First!
-              </h1>
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="/home" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
             </div>
-      
-            {/* Email Input */}
-            <div className="w-full p-2 flex justify-center">
-              <div className="w-full max-w-sm">
-                <Label>Email</Label>
-                <Input type="email" placeholder="Email" className="w-full" onChange={(e) => setUserEmail(e.target.value)} value={email} />
-              </div>
-            </div>
-      
-            {/* Password Input */}
-            <div className="w-full p-2 flex justify-center">
-              <div className="w-full max-w-sm">
-                <Label>Password</Label>
-                <Input type="password" placeholder="Password" className="w-full" onChange={(e) => setPassword(e.target.value)} value={password}/>
-              </div>
-            </div>
-      
-      {/* Hide forgot password functionality as of now */}
-            {/* Forgot Password Link */}
-            <div className="w-full flex flex-row-reverse p-5">
-              {/* <Label className="text-[var(--rs-primary-50)] hover:underline cursor-pointer ">
-                Forgot Password?
-              </Label> */}
-            </div>
-      
-            {/* Login Button */}
-            <div className="w-full p-5 flex justify-center">
-            <Button className="w-full max-w-sm"
-            onClick={handleLogin}
-            >
-                Login</Button>
-            </div>
-      
-
- {/* <div className="w-full p-5 flex justify-center items-center">
-                        <hr className="w-full border-t border-gray-300" />
-                        <span className="px-2 text-gray-500">OR</span>
-                        <hr className="w-full border-t border-gray-300" />
-                    </div>
-
-       
-                    <div className="w-full p-5 flex flex-col items-center space-y-3">
-                        <div className="w-full max-w-sm">
-                            <GoogleLoginButton onClick={() => alert("Google Login")} />
-                        </div>
-                        <div className="w-full max-w-sm">
-                            <FacebookLoginButton onClick={() => alert("Facebook Login")} />
-                        </div>
-                    </div> */}
-
-        
-            <h1 className="text-center">
-            Already have an account?  
-            <a href="/register" className="text-[var(--rs-primary-50)] hover:underline cursor-pointer ml-1">
-              Register Here
-            </a>
-          </h1>
-  
-            </div>
-
-            </div>
-
-          {/* RIGHT SIDE OF THE COLUMN (Image) */}
-      <div className="w-full">
-        <Image 
-          src="/aavc_right_column.jpg" // Ensure the image is placed in `public/`
-          alt="AAVC Right Column"
-          width={1500} // Adjust width as needed
-          height={1500} // Adjust height as needed
-          className="object-cover rounded-lg shadow-lg"
+            AAVCLAB
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/aavc_left_column.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
-        </div>
-    )
+    </div>
+  )
 }
--- src/app/(unauth)/(credentials)/register/page.tsx
+++ src/app/(unauth)/(credentials)/register/page.tsx
@@ -1,151 +1,33 @@
-'use client'
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Image from "next/image";
-import { useState } from "react";
-import { userRegister } from "@/app/endpoint/api";
-
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-
+import { SignUpForm } from "@/components/SignUpForm"
+//import { LoginForm } from "@/components/WhoWeAre"
+import { GalleryVerticalEnd } from "lucide-react"
 export default function SignupPage() {
-
-  const [role, setRole] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  
-  const handleRegister = () =>{
-    userRegister(role, email, password)
+    return (
+      <div className="grid min-h-svh lg:grid-cols-2">
+             <div className="relative hidden bg-muted lg:block">
+          <img
+           src="/aavc_right_column.jpg"
+            alt="Image"
+            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          />
+        </div>
+        <div className="flex flex-col gap-4 p-6 md:p-10">
+          <div className="flex justify-center gap-2 md:justify-start">
+            <a href="#" className="flex items-center gap-2 font-medium">
+              <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                <GalleryVerticalEnd className="size-4" />
+              </div>
+              AAVCLAB
+            </a>
+          </div>
+          <div className="flex flex-1 items-center justify-center">
+            <div className="w-full max-w-xs">
+              <SignUpForm />
+            </div>
+          </div>
+        </div>
+   
+      </div>
+    )
   }
   
-  return (
-    <div className="flex items-center justify-center m-20">
-      {/* LEFT SIDE - IMAGE */}
-      <div className="w-full flex items-center justify-center">
-        <Image 
-          src="/aavc_left_column.jpg" // Ensure the image is in `public/`
-          alt="AAVC Left Column"
-          width={1500} 
-          height={1500}
-          className="object-cover rounded-lg shadow-lg"
-        />
-      </div>
-
-      {/* RIGHT SIDE - SIGN UP FORM */}
-      <div className="w-full min-w-lg flex items-center justify-center">
-        <div className="w-full flex flex-col items-center justify-center p-10">
-          
-          {/* Logo */}
-          <Image 
-            src="/logo-v2.png"
-            alt="AAVC Logo"
-            width={300}
-            height={200}
-            className="object-cover"
-          />
-
-          {/* Header */}
-          <div className="w-full p-5 text-center">
-            <h1 className="text-3xl md:text-4xl font-bold">
-              Create Your AAVC Lab Account!
-            </h1>
-          </div>
-
-          {/* Email Input */}
-          <div className="w-full p-5 flex justify-center">
-            <div className="w-full max-w-sm">
-              <Label>Email</Label>
-              <Input type="email" placeholder="Enter your email" className="w-full" 
-                onChange={(e) => setEmail(e.target.value)} value={email}
-              />
-            </div>
-          </div>
-          <div className="w-full p-5  justify-center flex flex-row">
-            <div className=" max-w-sm ">
-              <Label>First Name</Label>
-              <Input type="name" placeholder="Enter your First Name" className="w-full" 
-                onChange={(e) => setEmail(e.target.value)} value={email}
-              />
-        </div>
-       
-            <div className=" max-w-sm">
-              <Label>Last Name</Label>
-              <Input type="name" placeholder="Enter your Last Name" className="w-full" 
-                onChange={(e) => setEmail(e.target.value)} value={email}
-              />
-            </div>
-          </div>
-          {/* Password Input */}
-          <div className="w-full p-5 flex justify-center">
-            <div className="w-full max-w-sm">
-              <Label>Password</Label>
-              <Input type="password" placeholder="Create a password" className="w-full"
-              onChange={(e) => setPassword(e.target.value)} value={password}
-              />
-            </div>
-          </div>
-
-          {/* Confirm Password Input */}
-          <div className="w-full p-5 flex justify-center">
-            <div className="w-full max-w-sm">
-              <Label>Confirm Password</Label>
-              <Input type="password" placeholder="Confirm your password" className="w-full" />
-            </div>
-
-
-
-
-
-          </div>
-                   {/* Role Selection */}
-                   <div className="w-full p-5 flex justify-center">
-            <div className="w-full max-w-sm">
-              <Label>Role</Label>
-              <Select value={role} onValueChange={setRole}>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a Role" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Role</SelectLabel>
-                    <SelectItem value="user">User</SelectItem>
-                    <SelectItem value="researcher">Researcher</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-  
-
-          {/* Sign Up Button */}
-          <div className="w-full p-5 flex justify-center">
-            <Button className="w-full max-w-sm bg-[var(--rs-primary-900)] text-white hover:bg-[var(--rs-primary-50)]"
-              onClick={handleRegister}
-            >
-              Sign Up
-            </Button>
-          </div>
-
-          {/* Already have an account? */}
-          <h1 className="text-center">
-            Already have an account?  
-            <a href="/login" className="text-[var(--rs-primary-50)] hover:underline cursor-pointer ml-1">
-              Login Here
-            </a>
-          </h1>
-        </div>
-      </div>
-    </div>
-  );
-}
-
