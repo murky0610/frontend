3fad039ef86fce3b8b4bbbf1cb56d0045e89317d
murky0610 <murkymurky0@gmail.com>
2025-04-06 21:39:28 +0800

    feat: added error handling for login and register

--- src/app/(unauth)/(credentials)/login/page.tsx
+++ src/app/(unauth)/(credentials)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { GalleryVerticalEnd } from "lucide-react"
-import { LoginForm } from "@/components/LoginForm"
+import { LoginForm } from "@/components/client/LoginForm"
 
 
 export default function LoginPage() {
