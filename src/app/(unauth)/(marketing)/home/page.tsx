a5af1498e76f8d9f48632b5e5f98bab495e80531
murky0610 <murkymurky0@gmail.com>
2025-04-06 21:40:01 +0800

    added landing page

--- src/app/(unauth)/(marketing)/home/page.tsx
+++ src/app/(unauth)/(marketing)/home/page.tsx
@@ -1,11 +1,11 @@
-import { SignUpForm } from "@/components/SignUpForm"
+import { SignUpForm } from "@/components/client/SignUpForm"
 //import { LoginForm } from "@/components/WhoWeAre"
 import { GalleryVerticalEnd } from "lucide-react"
+import LandingPage from "@/components/client/LandingPage"
+import GoogleMapComponent from "@/components/GoogleMapComponent"
 export default function HomePage() {
     return (
-      <div>
-        This is home
-      </div>
+      <LandingPage/>
     )
   }
   
