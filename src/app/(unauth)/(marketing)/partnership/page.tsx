3232a852de6b41d2f7092e0750b07845e8db3913
murky0610 <murkymurky0@gmail.com>
2025-04-14 22:19:50 +0800

    fix: dummy values for parternship and service page

---
+++ src/app/(unauth)/(marketing)/partnership/page.tsx
@@ -0,0 +1,9 @@
+
+export default function PartnershipPage() {
+    return (
+      <div>
+        This is Partnership Page
+      </div>
+    )
+  }
+  
---
+++ src/app/(unauth)/(marketing)/services/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ServicesPage() {
+    return (
+      <div>
+        This is Services Page
+      </div>
+    )
+  }
+  
