a0051030689176ee81c335ee908b35d4dfcb47d5
murky0610 <murkymurky0@gmail.com>
2025-06-09 09:27:21 +0800

    refactor: added correct ref of home

--- src/app/(unauth)/(credentials)/register/page.tsx
+++ src/app/(unauth)/(credentials)/register/page.tsx
@@ -14,7 +14,7 @@ export default function SignupPage() {
         </div>
         <div className="flex flex-col gap-4 p-6 md:p-10">
           <div className="flex justify-center gap-2 md:justify-start">
-            <Link href="#" className="flex items-center gap-2 font-medium">
+            <Link href="/home" className="flex items-center gap-2 font-medium">
               <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
                 <GalleryVerticalEnd className="size-4" />
               </div>
